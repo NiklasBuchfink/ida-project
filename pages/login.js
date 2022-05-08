2b9ea8e73fb47de2fa1578ea834b5b9e3a3da83f
Maxframe <max.werner@stud.hslu.ch>
2022-05-09 01:08:47 +0200

    fix

--- pages/login.js
+++ pages/login.js
@@ -2,15 +2,19 @@ import { getProviders, signIn } from "next-auth/react";
 
 function Login({ providers }) {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full space-y-4">
       <h1>Login Page</h1>
 
-      {Object.values(providers).map((provider => (
-          <div key={provider.name}>
-            <button>Login with {provider.name}</button>
-          </div>
-        ))
-      )}
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-green-500 text-white p-5 rounded-md"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
