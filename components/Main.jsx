37c4645749dd8998d4c42754021818d77695265f
Maxframe <wrnerm@gmail.com>
2022-05-12 00:50:21 +0200

    CORS fixed (one time)

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,25 +6,56 @@ export default function Main() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
   const [playlists, setPlaylists] = useState([]);
+  const [topArtists, setTopArtists] = useState([]);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
-      spotifyApi.getUserPlaylists().then((data) => {
-        setPlaylists(data.body.items);
-      }, (err) => {
-        console.log("Something went wrong!", err);
-      });
+      spotifyApi.getUserPlaylists().then(
+        (data) => {
+          setPlaylists(data.body.items);
+        },
+        (err) => {
+          console.log("Something went wrong!", err);
+        }
+      );
+
+      spotifyApi.getMyTopArtists().then(
+        (data) => {
+          setTopArtists(data.body.items)
+        },
+        (err) => {
+          console.log("Something went wrong!", err);
+        }
+      );
     }
   }, [session, spotifyApi]);
 
   console.log(session);
   console.log(playlists);
+  console.log(topArtists)
 
   return (
     <div className="">
       <h1>Main element</h1>
       <p>{session?.user.name}</p>
-      <img className="w-10 h-10" src={session?.user.image} alt="Profile pic"></img>
+      <img
+        className="w-10 h-10"
+        src={session?.user.image}
+        alt="Profile pic"
+      ></img>
+
+      <p>Playlists</p>
+      {playlists.map((playlist) => {
+        <p key={playlist.id} className="cursor-pointer">
+          {playlist.name}
+        </p>;
+      })}
+            <p>Top Artists</p>
+      {topArtists.map((topArtist) => {
+        <p key={topArtist.id} className="cursor-pointer">
+          {topArtist.name}
+        </p>;
+      })}
     </div>
   );
 }
--- pages/app.js
+++ pages/app.js
@@ -1,28 +1,28 @@
-import { signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import Main from '../components/Main';
-
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Main from "../components/Main";
 
 export default function App() {
   const { data: session, status } = useSession();
   const router = useRouter();
-  
+
   const handleLogout = async () => {
-    const data = await signOut({redirect: false, callbackUrl: "/"})
-    router.push(data.url)
-  }
-  
-  
+    const data = await signOut({ redirect: false, callbackUrl: "/" });
+    router.push(data.url);
+  };
+
   return (
-    <div className="m-4 space-y-4">
-      <h1>App Page</h1>
-      <button 
-        onClick={handleLogout}
-        className="bg-red-500 text-white px-4 py-2 rounded-md w-fit"
-      >
-        Logout
-      </button>
+    <div className="m-4 space-y-6">
+      <div className="flex justify-between">
+        <h1>App Page</h1>
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded-md w-fit"
+        >
+          Logout
+        </button>
+      </div>
       <Main />
     </div>
-  )
+  );
 }
