776ea746fe5f5c5fc3b9d8833a3d3cb61bf06245
Niklas.Buchfink <niklas.buchfink@web.de>
2022-05-17 13:51:57 +0200

    get playlist, topartist, toptracks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,77 +1,25 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
-
-async function refreshAccessToken(token) {
-  try {
-    spotifyApi.setAccessToken(token.accessToken);
-    spotifyApi.setRefreshToken(token.refreshToken);
-    
-    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
-    console.log("refreshed token is", refreshedToken);
-    return {
-      ...token,
-      accessToken: refreshedToken.access_token,
-      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, // = 1 hour as 3600 returns from spotify API
-      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
-    };
-  } catch (error) {
-    console.error(error);
-    
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
-      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      authorization: LOGIN_URL,
+      authorization:
+        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-top-read',
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
-  secret: process.env.JWT_SECRET,
-  pages: {
-    signIn: "/login",
-  },
+
   callbacks: {
-    async jwt({ token, account, user }) {
-      // inital sign in
-      if (account && user) {
-        return {
-          ...token,
-          accessToken: account.access_token,
-          refreshToken: account.refresh_token,
-          username: account.providerAccountId,
-          accessTokenExpires: account.exprires_at * 1000, // in milliseconds
-        };
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.refresh_token;
       }
-
-      // Return previous token if the acces token has not expired yet
-      if (Date.now() < token.accessTokenExpires) {
-        console.log("existing token is valid");
-        return token;
-      }
-
-
-      let time = Date.now()
-
-      console.log(time)
-
-      console.log("token has expired, refreshing...");
-      return await refreshAccessToken(token);
+      return token;
     },
-
-    async session({ session, token }) {
-      session.user.accessToken = token.accessToken;
-      session.user.refreshToken = token.refreshToken;
-      session.user.username = token.username;
-
+    async session(session, user) {
+      session.user = user;
       return session;
     },
   },
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,99 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function Home() {
-  const { status } = useSession();
+  const { data: session } = useSession();
+  const [playlistsList, setPlaylistsList] = useState([]);
+  const [topArtistsList, setTopArtistsList] = useState([]);
+  const [topTracksList, setTopTracksList] = useState([]);
 
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="A Taste of Music" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <h1 className="text-3xl m-4">
-          A Taste of Music
-        </h1>
-        <div className="m-4">
-          {status === "authenticated"
-            ? (
-              <Link href="/app">
-              <div className="bg-red-500 text-white px-4 py-2 rounded-md w-fit cursor-pointer">
-                See my results
-              </div>
-            </Link>
-            ) : (
-              <Link href="/login">
-                <div className="bg-green-500 text-white px-4 py-2 rounded-md w-fit cursor-pointer">
-                  Discover
-                </div>
-              </Link>
-            )
-          }
+  const getMyPlaylists = async () => {
+    const res = await fetch('/api/playlists');
+    const { items } = await res.json();
+    setPlaylistsList(items);
+  };
+
+  const getMyTopArtists = async () => {
+    const res = await fetch('/api/topartists');
+    const { items } = await res.json();
+    setTopArtistsList(items);
+  };
+
+  const getMyTopTracks = async () => {
+    const res = await fetch('/api/toptracks');
+    const { items } = await res.json();
+    setTopTracksList(items);
+    console.log(items)
+  };
+
+
+  if (session) {
+    return (
+      <div className="m-4 space-y-4">
+        <Head>
+          <title>Create Next App</title>
+          <meta name="description" content="A Taste of Music" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        Signed in as {session?.token?.email} <br />
+        <button  onClick={() => signOut()} className="bg-red-500 text-white px-4 py-2 rounded-md w-fit cursor-pointer">Sign out</button>
+        <hr />
+        <div className="flex gap-4">
+          <button onClick={() => getMyPlaylists() }>Get all my playlists</button>
+          <button onClick={() => getMyTopArtists() }>Get all my topartists</button>
+          <button onClick={() => getMyTopTracks() }>Get all my toptracks</button>
         </div>
-      </Layout>
+        <div className="flex gap-4">
+          <div className="space-y-4">
+            {playlistsList.map((item) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <Image 
+                  src={item.images[0]?.url} 
+                  alt={item.name}
+                  width={96}
+                  height={96}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="space-y-4">
+            {topArtistsList.map((item) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <Image 
+                  src={item.images[0]?.url} 
+                  alt={item.name}
+                  width={96}
+                  height={96}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="space-y-4">
+            {topTracksList.map((item) => (
+              <div key={item.id}>
+                <p>{item.name} </p>
+                {/* {item.artists.map((name) => <span key={name.id}>{name}</span>)} */}
+                {/* <Image 
+                  src={item.images[0]?.url} 
+                  alt={item.name}
+                  width={96}
+                  height={96}
+                /> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="m-4 space-y-4">
+      Not signed in <br />
+      <button onClick={() => signIn()} className="bg-green-500 text-white px-4 py-2 rounded-md w-fit cursor-pointer">Sign in</button>
     </div>
-  )
+  );
 }
