590ad7f6d04629378474fcc93f4ef891f098b888
Niklas.Buchfink <niklas.buchfink@web.de>
2022-05-31 16:31:24 +0200

    get subgenre

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,15 +1,19 @@
-import { getSearch, getPlaylistItems, getTracksAudioFeatures } from '../../lib/spotify';
+import { getSpotify } from '../../lib/spotify';
 import { getSession } from 'next-auth/react';
 
 const handler = async (req, res) => {
   const {
     token: { accessToken },
   } = await getSession({ req });
-  // search for playlistId
+
   let playlistArray = []
   // 2009
   for (let i = new Date().getFullYear()-1; i > 2020 ; i--) {
-    let { data } = await getSearch(accessToken, 'Top Songs ' + i);
+    let { data } = await getSpotify(accessToken, "https://api.spotify.com/v1/search", {
+        q: 'Top Songs ' + i,
+        type: "playlist",
+        limit: 5
+    });
     for (let j = 0; j < data.playlists.items.length; j++) {
       if (
           data.playlists.items[j].owner.uri === "spotify:user:spotify"
@@ -26,41 +30,143 @@ const handler = async (req, res) => {
   }
 
   await Promise.all(playlistArray.map(async (playlist) => {
-    let { data } = await getPlaylistItems(accessToken, playlist.id);
+    let { data } = await getSpotify(accessToken, `https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
+        playlist_id: playlist.id,
+        fields: "items(track(id, name, artists, album, preview_url))",
+        limit: 100
+    })
     playlist.tracks = data.items;
   }));
 
   await Promise.all(playlistArray.map(async (playlist) => {
+    // Audio Features
     let features = {
-      "danceability": null,
-      "energy": null,
-      "valence": null,
+      "danceability": 0,
+      "energy": 0,
+      "valence": 0,
+      "tempo": 0
     }
-    let danceabilitySum = 0
-    let energySum = 0
-    let valenceSum = 0
-    let tempoSum = 0
-    await Promise.all(playlist.tracks.map(async (track) => {
-      let { data } = await getTracksAudioFeatures(accessToken, track.track.id);
-      danceabilitySum += data.danceability
-      energySum += data.energy
-      valenceSum += data.valence
-      tempoSum += data.tempo;
-      track.track.features = {
-        danceability: data.danceability,
-        energy: data.energy,
-        valence: data.valence,
-        tempo: data.tempo
-      }
-    }));
-    let tracksNum = playlist.tracks.length
-    features.danceability = danceabilitySum / tracksNum
-    features.energy = energySum / tracksNum
-    features.valence = valenceSum / tracksNum
-    features.tempo = tempoSum / tracksNum
-    playlist.features = features
-  }))
 
+    let trackIds = [];
+    playlist.tracks.map( (track) => {
+        trackIds.push(track.track.id);
+    });
+        
+    let { data } = await getSpotify(accessToken, "https://api.spotify.com/v1/audio-features/", { ids: trackIds.join(',') })
+    
+    playlist.tracks.map( (track, index) => {
+      features.danceability += data.audio_features[index].danceability
+      features.energy += data.audio_features[index].energy
+      features.valence += data.audio_features[index].valence
+      features.tempo += data.audio_features[index].tempo;
+      track.track.features = {
+        danceability: data.audio_features[index].danceability,
+        energy: data.audio_features[index].energy,
+        valence: data.audio_features[index].valence,
+        tempo: data.audio_features[index].tempo
+      }
+    });
+    let tracksNum = playlist.tracks.length
+    features.danceability = features.danceability / tracksNum
+    features.energy = features.energy / tracksNum
+    features.valence = features.valence / tracksNum
+    features.tempo = features.tempo / tracksNum
+    playlist.features = features
+
+    // Genre
+    let albumIds = [];
+    let artistIds = [];
+    let trackArtistsLength = [];
+    playlist.tracks.map( (track) => {
+      albumIds.push(track.track.album.id)
+      trackArtistsLength.push(track.track.artists.length)
+      track.track.artists.map( (artist) => {
+        artistIds.push(artist.id)
+      })
+    })
+
+    let albumGenre = [];
+    for (let i = 0; Math.floor(albumIds.length / 20); i++) {
+      let { data } = await getSpotify(accessToken, "https://api.spotify.com/v1/albums", { ids: albumIds.splice(0,20).join(',') })
+      data.albums.map( (album)=> {
+        albumGenre.push(album.genres)
+      })      
+    }
+
+    let artistGenre = [];
+    while (artistIds.length > 0) {
+      if (artistIds.length <= 50) {
+        let { data } = await getSpotify(accessToken, "https://api.spotify.com/v1/artists", { ids: artistIds.splice(0,artistIds.length).join(',') })
+        data.artists.map( (artist)=> {
+          artistGenre.push(artist.genres)
+        })
+        break;
+      }
+      let { data } = await getSpotify(accessToken, "https://api.spotify.com/v1/artists", { ids: artistIds.splice(0,50).join(',') })
+      data.artists.map( (artist)=> {
+        artistGenre.push(artist.genres)
+      })
+    }
+
+    playlist.tracks.map( (track, index) => {
+      let allGenres = [];
+      albumGenre[index].map( (genre) => {
+        allGenres.push(genre);
+      })
+
+      for (let i = 0; i < trackArtistsLength[index]; i++) {
+        artistGenre[0].map( (genre) => {
+          allGenres.push(genre)
+        })
+        artistGenre.shift()
+      }
+
+      if (allGenres.length === 0) {
+        allGenres.push('unclassified genre')
+      }
+
+      track.track.genre = {
+        main: null,
+        sub: null,
+        all: allGenres
+      }
+    })
+    
+    let playlistGenre = {
+      main: {},
+      sub: {}
+    }
+
+    playlist.tracks.map( (track) => {
+      track.track.genre.all.map( (trackGenre) => {
+        if (playlistGenre.sub.hasOwnProperty(trackGenre)) {
+          playlistGenre.sub[trackGenre]++
+        } else {
+          playlistGenre.sub[trackGenre] = 1
+        }
+      })
+    })
+
+    function sortObject(obj) {
+      let arr = [];
+      for (let prop in obj) {
+          if (obj.hasOwnProperty(prop)) {
+              arr.push({
+                  'genre': prop,
+                  'value': obj[prop]
+              });
+          }
+      }
+      arr.sort(function(a, b) { return b.value - a.value; });
+      //arr.sort(function(a, b) { a.value.toLowerCase().localeCompare(b.value.toLowerCase()); }); //use this to sort as strings
+      return arr;
+    }
+
+    playlistGenre.sub = sortObject(playlistGenre.sub)
+    playlist.genre = playlistGenre
+
+  }))
+  
   return res.status(200).json({ items: playlistArray });
 };
 
